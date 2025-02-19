bedb4b509f928d0c235326977c064df88e97f889
xiayu <3389282692@qq.com>
2025-02-19 22:42:34 +0800

    完成退出登录确认对话框

--- src/pages/Layout/components/TopBar.jsx
+++ src/pages/Layout/components/TopBar.jsx
@@ -1,40 +1,50 @@
 import React, { useState } from 'react'
-import { Breadcrumb } from 'antd'
-import { MenuFoldOutlined, MenuUnfoldOutlined } from '@ant-design/icons'
-import { Button, Layout as AntdLayout, Dropdown } from 'antd'
+import { Breadcrumb, Popconfirm, Dropdown } from 'antd'
+
+import { Modal, Button, Layout as AntdLayout } from 'antd'
 
 import autor from '@/assets/autor.jpeg'
-
-const items = [
-  {
-    key: '1',
-    label: (
-      <a target="_blank" rel="noopener noreferrer" href="#">
-        切换账号
-      </a>
-    ),
-  },
-  {
-    key: '2',
-    label: (
-      <a target="_blank" rel="noopener noreferrer" href="#">
-        退出登录
-      </a>
-    ),
-  },
-]
+import { useNavigate } from 'react-router-dom'
 
 const TopBar = () => {
   const [collapsed, setCollapsed] = useState(false)
+  const navigate = useNavigate()
+  const [open, setOpen] = useState(false)
+  const tiaozhuan = () => {
+    navigate('/login')
+  }
+  const showModal = e => {
+    e.preventDefault()
+    setOpen(true)
+  }
+
+  const hideModal = () => {
+    setOpen(false)
+  }
+  const dropdownItems = [
+    {
+      key: '1',
+      label: (
+        <a href="" onClick={showModal}>
+          切换账号
+        </a>
+      ),
+    },
+    {
+      key: '2',
+      label: (
+        <a href="" onClick={showModal}>
+          退出登录
+        </a>
+      ),
+    },
+  ]
 
   return (
     <>
-      
       <Breadcrumb
         style={{
-          // color: '#fff',
           margin: 'auto 24px',
-          // fontSize: '20px',
           fontSizeIcon: '20px',
         }}
         items={[
@@ -43,12 +53,10 @@ const TopBar = () => {
           },
           {
             title: ':id',
-
             href: '',
           },
           {
             title: ':id',
-
             href: '',
           },
         ]}
@@ -59,15 +67,19 @@ const TopBar = () => {
       <div style={{ height: '64px', marginLeft: 'auto', marginRight: '24px', display: 'flex', alignItems: 'center' }}>
         <Dropdown
           menu={{
-            items,
+            items: dropdownItems,
           }}
           placement="bottomRight"
           arrow
         >
           <img src={autor} alt="" style={{ width: '50px', height: '50px', borderRadius: '8px', cursor: 'pointer' }} />
         </Dropdown>
+        <Modal title="温馨提示" open={open} onOk={tiaozhuan} onCancel={hideModal} okText="确认" cancelText="取消">
+          <p>确定要退出登录吗</p>
+        </Modal>
       </div>
     </>
   )
 }
+
 export default TopBar
